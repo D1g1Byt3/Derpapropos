b451bae9afb6d03bf5d0bc6d17dbb7b980e2c70f
D1g1Byt3 Diamond <D1g1byt3@gmail.com>
2024-03-08 12:50:49 -0800

    Added Metal Detector custom Item.

--- src/main/java/net/d1g1byt3/derpapropos/item/ModItems.java
+++ src/main/java/net/d1g1byt3/derpapropos/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.d1g1byt3.derpapropos.item;
 
 import net.d1g1byt3.derpapropos.DerpAproposMod;
+import net.d1g1byt3.derpapropos.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +16,10 @@ public class ModItems {
             () -> new Item (new Item.Properties()));
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
             () -> new Item (new Item.Properties()));
+
+
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+            () -> new MetalDetectorItem(new Item.Properties()));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
