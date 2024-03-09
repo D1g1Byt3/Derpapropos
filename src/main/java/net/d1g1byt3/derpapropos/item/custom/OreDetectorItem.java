f7d8991e8b4e62bf393fba5ef9a65276e024c70f
D1g1Byt3 Diamond <D1g1byt3@gmail.com>
2024-03-08 17:33:20 -0800

    Changed Metal Detector to Ore Detector, added custom tags.

--- src/main/java/net/d1g1byt3/derpapropos/item/custom/OreDetectorItem.java
+++ src/main/java/net/d1g1byt3/derpapropos/item/custom/OreDetectorItem.java
@@ -13,7 +13,6 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
